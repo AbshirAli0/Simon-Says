23cfa2bfa5f629141b2e3bea3de9632bf0835413
Abshir-Ali <abshirali1001@gmail.com>
2024-05-04 15:30:02 -0400

    landingPage visible and button to start game is working

---
+++ app.js
@@ -0,0 +1,17 @@
+class Player{
+    constructor(name){
+        this.name = name
+        this.isAlive = true
+        this.timer = 0
+    }
+}
+
+let yourPlayer
+
+const startGame = () => {
+    const playerName = document.getElementById("name").value
+    yourPlayer = new Player(playerName)
+    document.getElementById("landingPage").style.display = "none";
+    document.getElementById("game-container").style.display="block";
+
+}

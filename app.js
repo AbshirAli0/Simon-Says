4bbc75b74f790a1653947d8644e66b9cf0563a70
Abshir-Ali <abshirali1001@gmail.com>
2024-05-05 20:16:22 -0400

    changes in CSS layout

--- app.js
+++ app.js
@@ -12,6 +12,6 @@ const startGame = () => {
     const playerName = document.getElementById("name").value
     yourPlayer = new Player(playerName)
     document.getElementById("landingPage-container").style.display = "none";
-    document.getElementById("game-container").style.display="block";
+    document.getElementById("game-container").style.display="flex";
 
 }

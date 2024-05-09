d89e5b682f9579bf4d299cacf96afa3bf224edba
Abshir-Ali <abshirali1001@gmail.com>
2024-05-08 20:01:08 -0400

    trying different things in HTML and JS to get the game running. Timer also connected now

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-class Player{
+class GameLogic{
     constructor(name){
         this.name = name
         this.isAlive = true
         this.timer = 0
+        this.timerInterval = null
+        this.timerDisplay = null
+    }
+    startTimer(){
+      this.timerDisplay = document.getElementById("timer")
+      this.timerInterval = setInterval(() => {
+        this.timer++;
+        this.updateTimerDisplay()
+      }, 1000);
+    }
+    updateTimerDisplay(){
+        const minutes = Math.floor(this.timer / 60)
+        const seconds = this.timer % 60
+        this.timerDisplay.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+    }
+    stopTimer(){
+        clearInterval(this.timerInterval)
     }
 }
 
@@ -17,6 +34,8 @@ const startGame = () => {
 }
 
 const beginGame = () => {
-    
+
    
 }
+
+//create
